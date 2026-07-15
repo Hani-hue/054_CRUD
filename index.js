3d4a1aae65acdcfa55979e78a3694db3d9abadd9
Hani-hue <isnahabibah9827@gmail.com>
2026-07-15 14:03:24 +0700

    inisialisasi server express dasar

---
+++ index.js
@@ -0,0 +1,44 @@
+import express from 'express'
+import pg from 'pg'
+const express = require('express');
+const {Pool} = require('pg');
+const app = express();
+const port = 3000;
+
+
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'mahasiswa',
+    password: 'IsnaHabibah123',
+    port: 5432,
+});
+
+app.use(express.json());
+
+
+app.get('/api/biodata', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM biodata');
+
+        res.status(200).json({
+            status: 'success',
+            pesan: 'Berhasil mengambil data biodata',
+            data: result.rows
+        });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).json({ error: 'Terjadi kesalahan pada server atau database' });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server berjalan di http://localhost:${port}`);
+});
+
+//post
+
+//put
+
+//delete
